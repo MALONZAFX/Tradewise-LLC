6f384dd320260c281e442aef78b6d18a5fef734a
Mesh <meshmwangi828@gmail.com>
2025-11-21 22:23:37 +0300

    Fresh

--- dict/settings.py
+++ dict/settings.py
@@ -27,6 +27,8 @@ ALLOWED_HOSTS = [
     "tradewise.up.railway.app",
     "127.0.0.1",
     "localhost",
+    "https://tradewise-hub.com",
+    "https://www.tradewise-hub.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
